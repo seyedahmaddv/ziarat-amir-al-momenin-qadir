4a3da22dc61db1cb8830090c0a054843788b4938
Seyed Ahmad <seyedahmaddv@gmail.com>
2024-06-25 06:25:20 +0330

    some optimize

--- ziarateamiralmomeninali/src/App.jsx
+++ ziarateamiralmomeninali/src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
-import MyText from './MyText'
+import MyText from './MyText.jsx'
 import haram from './assets/haram-najaf.jpg'
 
 function App() {
-  
-  return (
-    <>
-    <img src={haram} alt='حرم امیرالمومنین علی علیه السلام'>
-      <h1>زیارت امیرالمومنین علی علیه السلام در روز غدیر</h1>
-      
 
+  return (
+    <div>
+      <img src={haram} alt='حرم امیرالمومنین علی علیه السلام' />
+      <h1>زیارت امیرالمومنین علی علیه السلام در روز غدیر</h1>
       <MyText />
-    </>
+    </div>
   )
 }
 
