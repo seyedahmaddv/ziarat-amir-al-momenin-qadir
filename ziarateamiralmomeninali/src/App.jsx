23f8466a967ac413da3692ecd9166792db824662
Seyed Ahmad <seyedahmaddv@gmail.com>
2024-06-25 06:23:04 +0330

    add image

--- ziarateamiralmomeninali/src/App.jsx
+++ ziarateamiralmomeninali/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import MyText from './MyText'
-
+import haram from './assets/haram-najaf.jpg'
 
 function App() {
   
   return (
     <>
+    <img src={haram} alt='حرم امیرالمومنین علی علیه السلام'>
       <h1>زیارت امیرالمومنین علی علیه السلام در روز غدیر</h1>
       
 
