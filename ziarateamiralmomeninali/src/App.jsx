89898ac321a9d68b754e2d72a86c3b6a098e23b5
Seyed Ahmad <seyedahmaddv@gmail.com>
2024-06-25 04:25:56 +0330

    some changes

---
+++ ziarateamiralmomeninali/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>زیارت امیرالمومنین علی علیه السلام</h1>
+      
+    </>
+  )
+}
+
+export default App
