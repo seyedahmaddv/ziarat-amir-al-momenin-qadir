23a5c9982dd6cbcb83d3709608eeee3d39331604
Seyed Ahmad <seyedahmaddv@gmail.com>
2024-06-25 06:29:10 +0330

    optimise image

--- ziarateamiralmomeninali/src/App.jsx
+++ ziarateamiralmomeninali/src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <div>
-      <img src={haram} alt='حرم امیرالمومنین علی علیه السلام' />
+      <img src={haram} alt='حرم امیرالمومنین علی علیه السلام' width: '25%'
+  height: 'auto'/>
       <h1>زیارت امیرالمومنین علی علیه السلام در روز غدیر</h1>
       <MyText />
     </div>
